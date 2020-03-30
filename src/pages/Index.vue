2c4116e6fc422cdf212569ef8f1dc13183608503
danielmr <bdanielmr@gmail.com>
2020-03-30 02:23:52 -0500

    v

---
+++ src/pages/Index.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <EssentialLink title="hi" caption="" link="https://www.youtube.com/"/>
+  <QBUTTON/>
+</div>
+</template>
+
+<script>
+import EssentialLink from '../components/EssentialLink'
+import QBUTTON from '../../test/jest/__tests__/demo/QBtn-demo'
+export default {
+  name: 'PageIndex',
+  components: {
+    EssentialLink,
+    QBUTTON
+  }
+}
+</script>
---
+++ test/jest/__tests__/demo/QBtn-demo.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <p class="textContent">{{ input }}</p>
+  <span>{{ counter }}</span>
+  <br>
+  <q-btn label="increment" id="mybutton" @click="increment()"></q-btn>
+  <q-btn label="reset" @click="decrement(counter)"></q-btn>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'QBUTTON',
+  data () {
+    return {
+      counter: 0,
+      input: 'counter'
+    }
+  },
+  methods: {
+    increment () {
+      this.counter++
+    },
+    decrement (counter) {
+      this.counter = 0
+    }
+  }
+}
+</script>
